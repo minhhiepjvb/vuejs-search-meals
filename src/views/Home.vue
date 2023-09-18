e73835d0b0d7a46e3170993c613a5880025b022c
minhhiepjvb <thaiminhhiep.jvb@gmail.com>
2023-09-18 08:33:59 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex">
+        <input type="text" class="rounded border-2 border-grey-200">
+    </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+
+const meals = computed(() => store.state.meals)
+
+</script>
+
+<style></style>
